205b05f67d1f7f3cc42474ea4fb9cdcd69b36870
Bùi Hải Nghĩa <93520120+Hainghiabui@users.noreply.github.com>
2024-11-02 13:47:07 +0700

    login sv + gvhd

---
+++ Nhom_1_DotNet/FrmSinhVien.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Nhom_1_DotNet
+{
+    partial class FrmSinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(83, 45);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(140, 25);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Tên sinh viên: ";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthDate.Location = new System.Drawing.Point(94, 86);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(105, 25);
+            this.lblBirthDate.TabIndex = 1;
+            this.lblBirthDate.Text = "Ngày sinh:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(94, 175);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(132, 25);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Số điện thoại:";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(83, 134);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(88, 25);
+            this.lblGender.TabIndex = 2;
+            this.lblGender.Text = "Giới tính:";
+            // 
+            // FrmSinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblName);
+            this.Name = "FrmSinhVien";
+            this.Text = "FrmSinhVien";
+            this.Load += new System.EventHandler(this.FrmSinhVien_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblGender;
+    }
+}
